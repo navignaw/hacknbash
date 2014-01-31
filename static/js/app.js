9db624d9395da0c091abc768f3d6a99e3af244fe
Ivan Wang <icw@cmu.edu>
2014-01-31 01:14:10 -0500

    createjs magic begins

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,23 @@
-// Run this code when the page loads
-$(function() {
-    console.log("derp");
-});
+(function() {
+
+    window.onload = function() {
+        initialize();
+    }
+
+    var initialize = function() {
+        var canvas = document.getElementById("canvas");
+        var stage = new createjs.Stage(canvas);
+
+        var player = new createjs.Shape();
+        player.graphics.beginFill("red").drawCircle(0, 0, 40);
+        player.x = player.y = 50;
+        stage.addChild(player);
+
+        player.addEventListener("click", function(event) {
+            console.log("clicked on me!");
+        });
+
+        stage.update();
+    }
+
+})();
